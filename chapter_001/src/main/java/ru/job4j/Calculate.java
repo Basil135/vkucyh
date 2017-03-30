714e009c67e7fcd0718ccf093e7c8d7c63c73bf3
Basil135 <basil135@mail.ru>
2017-03-30 10:59:54 +0300

    added package-info.java and JavaDoc

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,17 @@
 package ru.job4j;
 
+/**
+* Class Calculate is the solution of the problem from package 001 lesson 1.
+* @author Kucyh Vasily
+* @since 29.03.2017
+*/
 public class Calculate {
 
+	/**
+	* Method main
+	* @param args
+	* @return void
+	*/
 	public static void main(String... args) {
 	
 		System.out.println("Hello world");
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,9 @@
+/**
+* Package for calculate task.
+*
+* @author Kucyh Vasily (mailto:basil135@mail.ru)
+* @version $Id$
+* @since 0.1
+*/
+
+package ru.job4j;
