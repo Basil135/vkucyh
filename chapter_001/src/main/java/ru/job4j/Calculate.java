9a3cf407a29e7b4ed72287c9c020f432d00b06f0
Basil135 <basil135@mail.ru>
2017-03-30 12:48:44 +0300

    Refactor Calculate.java

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -2,18 +2,19 @@ package ru.job4j;
 
 /**
 * Class Calculate is the solution of the problem from package 001 lesson 1.
-* @author Kucyh Vasily
+*
+* @author Kucyh Vasily (mailto:basil135@mail.ru)
 * @since 29.03.2017
 */
 public class Calculate {
 
 	/**
-	* Method main
-	* @param args
-	* @return void
+	* Method main.
+	*
+	* @param args - args
 	*/
 	public static void main(String... args) {
-	
+
 		System.out.println("Hello world");
 
 	}
