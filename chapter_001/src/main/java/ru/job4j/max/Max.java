309e04174e0766c8e3f0e30b015fdfe041bd5489
Basil135 <basil135@mail.ru>
2017-04-01 23:11:38 +0300

    Refactor Max.java

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -11,12 +11,25 @@ public class Max {
 
 	/**
 	* Method return max value.
+	*
 	* @param first is the first integer number
 	* @param second is the second integer number
 	* @return integer that is maximum from two numbers
 	*/
 	public int max(final int first, final int second) {
 		return first > second ? first : second;
+	}
+
+	/**
+	* This method return maximum number from three numbers.
+	*
+	* @param first is the first number
+	* @param second is the second number
+	* @param third is the third number
+	* @return maximum value
+	*/
+	public int max(final int first, final int second, final int third) {
+		return max(max(first, second), third);
 	}
 
 }
