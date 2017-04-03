4794a8251db0a4f968b5729b1a148bbe82f59d88
Basil135 <basil135@mail.ru>
2017-04-03 11:34:43 +0300

    Refactor chapter_001/src/main/java/ru/job4j/array/package-info.java

--- chapter_001/src/main/java/ru/job4j/array/package-info.java
+++ chapter_001/src/main/java/ru/job4j/array/package-info.java
@@ -1,5 +1,5 @@
 /**
-* This package contains classes to work with arrays, Turn is the class to turn back an array.
+* This package contains classes to work with arrays, Turn is the class to turn back an array; Class BubbleSort implements bubble sort.
 */
 
 package ru.job4j.array;
