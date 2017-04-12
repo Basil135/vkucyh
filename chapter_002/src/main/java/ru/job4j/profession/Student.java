ad93c2845ff501e1710e70c39bec080cacd54839
Basil135 <basil135@mail.ru>
2017-04-12 12:15:57 +0300

    Added Student.java

---
+++ chapter_002/src/main/java/ru/job4j/profession/Student.java
@@ -0,0 +1,21 @@
+package ru.job4j.profession;
+
+/**
+ * This class describes student with no parameters and operations.
+ *
+ * @author Kucykh Vasily (mailto:basil135@mail.ru)
+ * @version $Id$
+ * @since 12.04.2017
+ */
+public class Student extends Human {
+
+    /**
+     * constructor of Student class.
+     *
+     * @param name is name of a student
+     */
+    public Student(String name) {
+        super(name);
+    }
+
+}
