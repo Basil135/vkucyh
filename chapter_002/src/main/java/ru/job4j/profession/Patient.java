0cf85cfe49f01575e90e11b42280982707110966
Basil135 <basil135@mail.ru>
2017-04-12 12:14:17 +0300

    Added Patient.java

---
+++ chapter_002/src/main/java/ru/job4j/profession/Patient.java
@@ -0,0 +1,21 @@
+package ru.job4j.profession;
+
+/**
+ * this class describes simple patient without arguments and operations.
+ *
+ * @author Kucykh Vasily (mailto:basil135@mail.ru)
+ * @version $Id$
+ * @since 12.04.2017
+ */
+public class Patient extends Human {
+
+    /**
+     * constructor of Patient class.
+     *
+     * @param name is name of a patient
+     */
+    public Patient(String name) {
+        super(name);
+    }
+
+}
