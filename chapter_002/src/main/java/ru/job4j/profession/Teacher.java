c2e29a8b7e41632578e98724befd53d69582b92c
Basil135 <basil135@mail.ru>
2017-04-12 09:29:42 +0300

    Added Teacher.java

---
+++ chapter_002/src/main/java/ru/job4j/profession/Teacher.java
@@ -0,0 +1,66 @@
+package ru.job4j.profession;
+
+/**
+ * This class is class of profession teacher.
+ *
+ * @author Kucykh Vasily (mailto:basil135@mail.ru)
+ * @version $Id$
+ * @since 11.04.2017
+ */
+public class Teacher extends Profession {
+
+    /**
+     * This is constructor of teacher.
+     *
+     * @param name is the name of the teacher
+     * @param diploma is the diploma of the teacher
+     * @param speciality is the speciality of the teacher
+     * @param workExp is the work experience of the teacher
+     */
+    public Teacher(String name, String diploma, String speciality, int workExp) {
+        super(name, diploma, speciality, workExp);
+    }
+
+    /**
+     * This method teach a student.
+     *
+     * @param student is the name of a student
+     * @return just a string
+     */
+    public String teach(String student) {
+
+        String result;
+        StringBuilder builder = new StringBuilder();
+
+        builder.append(this.getName());
+        builder.append(" teach ");
+        builder.append(student);
+
+        result = builder.toString();
+
+        return result;
+
+    }
+
+    /**
+     * This method exam a student.
+     *
+     * @param student is the name of a student
+     * @return just a string
+     */
+    public String exam(String student) {
+
+        String result;
+        StringBuilder builder = new StringBuilder();
+
+        builder.append(this.getName());
+        builder.append(" examinate ");
+        builder.append(student);
+
+        result = builder.toString();
+
+        return result;
+
+    }
+
+}
