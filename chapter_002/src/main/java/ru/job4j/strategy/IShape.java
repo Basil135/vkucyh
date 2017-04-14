05a710f47a1916e42a16b6ccc69b9d847ea7ac7e
Basil135 <basil135@mail.ru>
2017-04-14 17:06:40 +0300

    Added IShape.java

---
+++ chapter_002/src/main/java/ru/job4j/strategy/IShape.java
@@ -0,0 +1,19 @@
+package ru.job4j.strategy;
+
+/**
+ * interface Shape generate string as a figure.
+ *
+ * @author Kucykh Vasily (mailto:basil135@mail.ru)
+ * @version $Id$
+ * @since 14.04.2017
+ */
+public interface IShape {
+
+    /**
+     * method generate string as a figure.
+     *
+     * @return string
+     */
+    String pic();
+
+}
