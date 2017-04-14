f82cde116f08635d5fe5a29933da5bb04d73f403
Basil135 <basil135@mail.ru>
2017-04-14 15:19:40 +0300

    Added StubInput.java

---
+++ chapter_002/src/main/java/ru/job4j/tracker/start/StubInput.java
@@ -0,0 +1,41 @@
+package ru.job4j.tracker.start;
+
+/**
+ * This class uses for test.
+ *
+ * @author Kucykh Vasily (mailto:basil135@mail.ru)
+ * @version $Id$
+ * @since 14.04.2017
+ */
+public class StubInput implements Input {
+
+    /**
+     * parameter answers uses to implement users input.
+     */
+    private String[] answers;
+    /**
+     * parameter index find need String from array of answers.
+     */
+    private int index = 0;
+
+    /**
+     * constructor of StubInput class.
+     *
+     * @param answers is array of answer
+     */
+    public StubInput(String[] answers) {
+        this.answers = answers;
+    }
+
+    /**
+     * method return answer.
+     *
+     * @param question is question
+     * @return answer
+     */
+    @Override
+    public String ask(String question) {
+        return answers[index++];
+    }
+
+}
