40bb16d968b95b5e1245676080a12cf136524a51
Basil135 <basil135@mail.ru>
2017-04-14 11:12:48 +0300

    Added ConsoleInput.java

---
+++ chapter_002/src/main/java/ru/job4j/tracker/start/ConsoleInput.java
@@ -0,0 +1,34 @@
+package ru.job4j.tracker.start;
+
+import java.util.Scanner;
+
+/**
+ * This class is implementation of interface input.
+ *
+ * @author Kucykh Vasily (mailto:basil135@mail.ru)
+ * @version $Id$
+ * @since 13.04.2017
+ */
+public class ConsoleInput implements Input {
+
+    /**
+     * this scanner is is scanner.
+     */
+    private Scanner scanner = new Scanner(System.in);
+
+    /**
+     * method ask a user about what to do next.
+     *
+     * @param question is question about what to do next
+     * @return String that we input
+     */
+    @Override
+    public String ask(String question) {
+
+        System.out.println(question);
+
+        return scanner.nextLine();
+
+    }
+
+}
