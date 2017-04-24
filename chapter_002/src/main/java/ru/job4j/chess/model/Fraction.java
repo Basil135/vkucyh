5a361b15780ad0b43be8b9ab0e2d009924ac3542
Basil135 <basil135@mail.ru>
2017-04-25 01:37:10 +0300

    Added Fraction.java

---
+++ chapter_002/src/main/java/ru/job4j/chess/model/Fraction.java
@@ -0,0 +1,15 @@
+package ru.job4j.chess.model;
+
+/**
+ * This class describes fraction of the chess application.
+ *
+ * @author Kucykh Vasily (mailto:basil135@mail.ru)
+ * @version $Id$
+ * @since 22.04.2017
+ */
+public enum Fraction {
+    /**
+     * fractions of the chess application.
+     */
+    white, black;
+}
