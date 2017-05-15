c0955091e7620a73c835d4495891630717ae8d3a
Basil135 <basil135@mail.ru>
2017-05-15 23:00:08 +0300

    Refactor exceptions added java doc

--- chapter_005/src/main/java/ru/job4j/collections/bank/exceptions/UnknownAccountException.java
+++ chapter_005/src/main/java/ru/job4j/collections/bank/exceptions/UnknownAccountException.java
@@ -1,7 +1,17 @@
 package ru.job4j.collections.bank.exceptions;
 
+/**
+ * This class is exception, if user vahe not searching account then it is throws.
+ *
+ * @author Kucykh Vasily (mailto:basil135@mail.ru)
+ * @version $Id$
+ * @since 15.05.2017
+ */
 public class UnknownAccountException extends Exception {
 
+    /**
+     * constructor of class.
+     */
     public UnknownAccountException() {
         super("There is no that account if this user.");
     }
--- chapter_005/src/main/java/ru/job4j/collections/bank/exceptions/UnknownUserException.java
+++ chapter_005/src/main/java/ru/job4j/collections/bank/exceptions/UnknownUserException.java
@@ -1,7 +1,17 @@
 package ru.job4j.collections.bank.exceptions;
 
+/**
+ * This class throws if user do not contains to collection.
+ *
+ * @author Kucykh Vasily (mailto:basil135@mail.ru)
+ * @version $Id$
+ * @since 15.05.2017
+ */
 public class UnknownUserException extends Exception {
 
+    /**
+     * constructor of class.
+     */
     public UnknownUserException() {
         super("There is no that user in this map");
     }
