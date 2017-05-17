bbd2d7b1847c06b44d363141c521f944e790c247
Basil135 <basil135@mail.ru>
2017-05-18 01:26:30 +0300

    Refactor Department.java

--- chapter_005/src/main/java/ru/job4j/collections/test/Department.java
+++ chapter_005/src/main/java/ru/job4j/collections/test/Department.java
@@ -40,12 +40,19 @@ public class Department {
      */
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
 
         Department that = (Department) o;
 
         return name.equals(that.name);
+
     }
 
     /**
