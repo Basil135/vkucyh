5a772b98e12a7da50b6b90733434671da350f969
Basil135 <basil135@mail.ru>
2017-05-24 16:28:41 +0300

    Added Queue.java

---
+++ chapter_005/src/main/java/ru/job4j/pro/list/Queue.java
@@ -0,0 +1,85 @@
+package ru.job4j.pro.list;
+
+/**
+ * This class describes collection queue.
+ *
+ * @param <E> is generic type
+ */
+public class Queue<E> {
+
+    /**
+     * parameter first is link to first element of the queue.
+     */
+    Node<E> first;
+    /**
+     * parameter last is link to last element of the queue.
+     */
+    Node<E> last;
+
+    /**
+     * method add element to the and of collection
+     *
+     * @param element
+     * @return
+     */
+    public boolean push(E element) {
+
+        Node<E> newNode = new Node<>();
+
+        newNode.value = element;
+
+        if (first == null) {
+            first = newNode;
+            last = newNode;
+        } else {
+            last.next = newNode;
+            last = newNode;
+        }
+
+        return true;
+
+    }
+
+    /**
+     * method delete last element and return value.
+     *
+     * @return value of last element
+     */
+    public E pop() {
+
+        E result = first.value;
+
+        first = first.next;
+
+        return result;
+
+    }
+
+    /**
+     * method return true if collection have no elements.
+     *
+     * @return true if collection is empty
+     */
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    /**
+     * inner class describes node of collection queue.
+     *
+     * @param <E> is generic type
+     */
+    private class Node<E> {
+
+        /**
+         * parameter next is link to next element of list.
+         */
+        Node<E> next;
+        /**
+         * parameter value is value of node.
+         */
+        E value;
+
+    }
+
+}
