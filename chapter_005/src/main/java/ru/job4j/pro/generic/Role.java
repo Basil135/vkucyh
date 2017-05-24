cfd548e12170dd500775efcd5164ac7d878bd51a
Basil135 <basil135@mail.ru>
2017-05-24 23:52:14 +0300

    Refactor Role.java

--- chapter_005/src/main/java/ru/job4j/pro/generic/Role.java
+++ chapter_005/src/main/java/ru/job4j/pro/generic/Role.java
@@ -43,8 +43,13 @@ public class Role extends Base {
      */
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
 
         Role role = (Role) o;
 
