30fc40dc6b2f72c2f79ee57c3f5cea61301044d1
Basil135 <basil135@mail.ru>
2017-05-24 16:22:29 +0300

    Added Base.java

---
+++ chapter_005/src/main/java/ru/job4j/pro/generic/Base.java
@@ -0,0 +1,26 @@
+package ru.job4j.pro.generic;
+
+/**
+ * This abstract class have teo abstract methods getId and setId.
+ *
+ * @author Kucykh Vasily (mailto:basil135@mail.ru)
+ * @version $Id$
+ * @version 22.05.2017
+ */
+public abstract class Base {
+
+    /**
+     * abstract method getId return id as A String
+     *
+     * @return String Id
+     */
+    abstract public String getId();
+
+    /**
+     * abstract method setId is setting id.
+     *
+     * @param id is identification string
+     */
+    abstract public void setId(String id);
+
+}
